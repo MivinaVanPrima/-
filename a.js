03395554ea5ba7bc3e6056c83be25277f1c5701f
MivinaVanPrima <135717348+MivinaVanPrima@users.noreply.github.com>
2023-06-06 19:26:00 +0500

    lab2

--- a.js
+++ a.js
@@ -1,52 +1,19 @@
-let a = parseInt(prompt("Input a"))
-let b = parseInt(prompt("Input b"))
-let c = parseInt(prompt("Input c"))
-let D = b*b - 4*a*c
-
-if (a != 0 && b != 0){
-	if (D > 0){
-		D = Math.sqrt(D)
-		let x1 = (-b - D) / (2*a)
-		let x2 = (-b + D) / (2*a)
-		let s = "x1 = " + String(x1) + "\n"
-		s += 	"x2 = " + String(x2)
-		alert(s)
+let s = '<table>' + "\n"
+s +=	"<tbody>" + "\n"
+s +=	"<tr>" + "\n"
+	s += 	"<th>" + " " + "</th>"
+	for (var i = 1; i <= 10; i++) {
+		s += "<th>" + String(i) + "</th>" + "\n"
 	}
-	else if (D == 0){
-		D = Math.sqrt(D)
-		let x = -b / (2*a)
-		let s = "x = " + String(x)
-		alert(s)
+	s += 	"</tr>" + "\n"
+for (var i = 1; i <= 10; i++) {
+	s +=	"<tr>" + "\n"
+	s += 	"<th>" + String(i) + "</th>"
+	for (var j = 1; j <= 10; j++) {
+		s += "<th>" + String(i*j) + "</th>" + "\n"
 	}
-	else if (D < 0){
-		let re = -b / 2 * a
-		let im = Math.sqrt(-1 * D)
-		let s = "x1 = " + String(re) + " + " + String(im) + "i" + "\n"
-		s += 	"x2 = " + String(re) + " - " + String(im) + "i"
-		alert(s)
-	}
+	s += 	"</tr>" + "\n"
 }
-else if (a != 0 && b == 0 && c != 0){
-	if (c < 0){
-		let x = -1 * c / a
-		x = Math.sqrt(x)
-		let s = "x1 = " + String(x) + "\n"
-		x *= -1
-		s +=	"x2 = " + String(x)
-		alert(s)
-	}
-	else if (c > 0){
-		let x = c / a
-		x = Math.sqrt(x)
-		let s = "x1 = " + String(x) + "i" + "\n"
-		x *= -1
-		s +=	"x2 = " + String(x) + "i"
-		alert(s)
-	}
-}
-else if (a != 0 && b == 0 && c == 0){
-	alert("x = 0")
-}
-else{
-	alert("No solution")
-}
+s +=	"</tbody>" + "\n"
+s += 	"</table>"
+tb.innerHTML = s
